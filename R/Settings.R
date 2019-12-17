ca32935688b973ca554596809e63302aa8594e60
Claudio Zandonella Callegher <claudiozandonella@gmail.com>
2019-12-17 16:04:26 +0100

    Add Settings
    *add packages list
    *update renv

---
+++ R/Settings.R
@@ -0,0 +1,12 @@
+
+################################
+####    Project settings    ####
+################################
+
+#----     R packages   ----
+library(conflicted)
+library(tidyverse)
+library(metafor)
+library(clubSandwich)
+library(knitr)
+library(kableExtra)
